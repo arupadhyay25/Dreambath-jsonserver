2fba3b8c64228ec31f9d1f47455ae99e520da893
Madhusudan Prajapat <103635175+Madhusudan0906@users.noreply.github.com>
2022-12-25 17:25:45 +0530

    Update db.js

--- config/db.js
+++ config/db.js
@@ -1,7 +1,7 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 mongoose.set('strictQuery', false);
-const MONGO_URL = process.env.MONGO_URL;
-const connect = mongoose.connect(`${MONGO_URL}`);
+// const MONGO_URL = process.env.MONGO_URL;
+const connect = mongoose.connect(`mongodb+srv://mad:cloud@cluster0.eygsg9e.mongodb.net/dreamBath`);
 
 module.exports={connect};
